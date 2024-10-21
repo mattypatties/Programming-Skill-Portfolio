1a63761c6855c07caeaf4e91c95549fdefddc426
matty <mattyrivera1421@gmail.com>
2024-10-21 12:39:02 +0400

    Add files via upload

--- exercise8(advanced).py
+++ exercise8(advanced).py
@@ -1,9 +1,9 @@
 """
 Exercise 8: Simple Search - 30 Marks advanced
 """
-names=["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"]
-search=input("Enter a name: ")
-if search in names:
+names=["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] #set a list
+search=input("Enter a name: ") #ask for user input
+if search in names: #check if the user input is in the list
     print(f"The name {search} was found in the list!")
-else:
+else: #if the input is not in the list provide feedback
     print(f"The name {search} was not found in the list.")
--- exercise8.py
+++ exercise8.py
@@ -1,9 +1,9 @@
 """
 Exercise 8: Simple Search - 30 Marks
 """
-names=["Jake" "Zac", "Ian", "Ron", "Sam", "Dave"]
-search=("Sam")
-if search in names:
+names=["Jake", "Zac", "Ian", "Ron", "Sam", "Dave"] #create a list
+search=("Sam") #fix a variable to "Sam"
+if search in names: #check if Sam is in the list
     print(f"The name {search} was found in the list!")
-else:
+else: #if Sam is not in the list, provide feedback
     print(f"The name {search} was not found in the list.")
